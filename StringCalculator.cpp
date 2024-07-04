4d8f3e1e753aeb82ec93904088538945e1b56844
ankegowda <60497313+ankegowdaja@users.noreply.github.com>
2024-07-04 12:46:22 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,1 +1,16 @@
+#include "StringCalculator.h"
+#include <sstream>
 
+int StringCalculator::add(const std::string& input) {
+    int sum = 0;
+    std::istringstream iss(input);
+    std::string num;
+
+    while (std::getline(iss, num, ',')) {
+        if (!num.empty()) {
+            sum += std::stoi(num);
+        }
+    }
+
+    return sum;
+}
