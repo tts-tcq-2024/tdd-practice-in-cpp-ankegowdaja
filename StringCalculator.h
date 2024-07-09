53ecb49a20ab32daddee201776c75af17199d1a9
ankegowda <60497313+ankegowdaja@users.noreply.github.com>
2024-07-09 18:43:53 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -7,9 +7,9 @@ class StringCalculator {
 public:
     int addnum(const std::string& input);
     int sumOfNumbers(const std::string& input);
-    void verifiy_negative_numbers(const std::string& input);
-    std::string Delimeterseperation(const std::string& input);
-    std::string newlinecheck(const std::string& input);   
+    void checkForNegativeNumbers(const std::string& input);
+    std::string normalizeDelimiters(const std::string& input);
+    std::string handleCustomDelimiter(const std::string& input);   
  };
 
 #endif 
