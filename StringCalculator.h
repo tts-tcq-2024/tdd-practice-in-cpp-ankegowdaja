1b984314a7a9ac00d6a00617a45d25d9eca07d50
ankegowda <60497313+ankegowdaja@users.noreply.github.com>
2024-07-09 18:35:53 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -5,12 +5,11 @@
 
 class StringCalculator {
 public:
+    int addnum(const std::string& input);
     int sumOfNumbers(const std::string& input);
     void verifiy_negative_numbers(const std::string& input);
     std::string Delimeterseperation(const std::string& input);
-    std::string newlinecheck(const std::string& input);
-    int addnum(const std::string& input);
-    
+    std::string newlinecheck(const std::string& input);   
  };
 
 #endif 
